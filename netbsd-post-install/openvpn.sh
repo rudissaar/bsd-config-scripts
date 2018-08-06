a04c4fdb7f8b586549b871c7937b06c20af1399c
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-08-06 16:15:48 +0000

    netbsd-post-install/openvpn.sh script development.

--- netbsd-post-install/openvpn.sh
+++ netbsd-post-install/openvpn.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 OPENVPN_DIR='/usr/pkg/etc/openvpn/'
+EASY_RSA_BIN='/usr/pkg/bin/easyrsa'
 
 if [ "$(id -u)" != '0' ]; then
     echo '> You need to become root to run this script.'
@@ -13,9 +14,11 @@ if [ "${?}" = '0' ]; then
     export PKG_PATH
 fi
 
+# Install packages.
 pkg_add -v \
-    openvpn \
-    easy-rsa
+    easy-rsa \
+    openssl \
+    openvpn
 
 if [ "$(sysctl -nw net.inet.ip.forwarding)" = '0' ]; then
     sysctl -w net.inet.ip.forwarding=1
@@ -28,7 +31,26 @@ if [ "${?}" != '0' ]; then
     echo 'net.inet.ip.forwarding=1' >> /etc/sysctl.conf
 fi
 
+# Create symbolic link to be more similar to Linux structure.
+if [ ! -L '/etc/openvpn' ] && [ ! -d '/etc/openvpn' ] ; then
+    ln -sf "${OPENVPN_DIR}" /etc/openvpn
+fi
+
+# Create directory for Easy RSA if if doesn't exist.
+if [ ! -d "${OPENVPN_DIR}easy-rsa" ]; then
+    mkdir -p "${OPENVPN_DIR}easy-rsa"
+fi
+
+# Copy Easy RSA files to OpenVPN directory.
+ln -sf "${EASY_RSA_BIN}" "${OPENVPN_DIR}easy-rsa/easyrsa"
+
+cp -r /usr/pkg/share/examples/easyrsa/* "${OPENVPN_DIR}easy-rsa/"
+
+# Move example configuration file to OpenVPN's directory.
 cp '/usr/pkg/share/examples/openvpn/config/server.conf' "${OPENVPN_DIR}"
 TMP_FILE=$(mktemp)
 egrep -v "^#|^;|^$" "${OPENVPN_DIR}server.conf" > "${TMP_FILE}"
 mv "${TMP_FILE}" "${OPENVPN_DIR}server.conf"
+
+# Generate TLS Auth key.
+openvpn --genkey --secret "${OPENVPN_DIR}ta.key"
