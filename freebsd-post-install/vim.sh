2806c72caaa96689c2c9cb4ed77befd48fa054e8
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-11-15 18:24:19 +0000

    Added freebsd-post-install/vim.sh script.

---
+++ freebsd-post-install/vim.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env sh
+
+# You need root permissions to run this script.
+if [ "$(id -u)" != '0' ]; then
+    echo '> You need to become root to run this script.'
+    exit 1
+fi
+
+# Install packages.
+pkg install -y vim-console
+
+grep -Fq 'alias vi' /etc/csh.cshrc
+
+if [ "${?}" != '0' ]; then
+    echo >> /etc/csh.cshrc
+    echo "alias vi vim" >> /etc/csh.cshrc
+    alias vi vim
+fi
+
+if [ ! -f /etc/skel/.vimrc ]; then
+    touch /etc/skel/.vimrc
+fi
+
+# Enable syntax.
+grep -Fq 'syntax on' /etc/skel/.vimrc
+
+if [ "${?}" != '0' ]; then
+    echo 'syntax on' >> /etc/skel/.vimrc
+fi
+
+# Display line numbers.
+grep -Fq 'set number' /etc/skel/.vimrc
+
+if [ "${?}" != '0' ]; then
+    echo 'set number' >> /etc/skel/.vimrc
+fi
+
+# Disable visual mode.
+grep -Fq 'set mouse-=a' /etc/skel/.vimrc
+
+if [ "${?}" != '0' ]; then
+    echo 'set mouse-=a' >> /etc/skel/.vimrc
+fi
+
+# Set size of tab.
+grep -Fq 'set tabstop=4' /etc/skel/.vimrc
+
+if [ "${?}" != '0' ]; then
+    echo 'set tabstop=4' >> /etc/skel/.vimrc
+fi
+
+# Use spaces instead of tabs.
+grep -Fq 'set expandtab' /etc/skel/.vimrc
+
+if [ "${?}" != '0' ]; then
+    echo 'set expandtab' >> /etc/skel/.vimrc
+fi
+
+# Set size of indent.
+grep -Fq 'set shiftwidth=4' /etc/skel/.vimrc
+
+if [ "${?}" != '0' ]; then
+    echo 'set shiftwidth=4' >> /etc/skel/.vimrc
+fi
+
+# Copy .vimrc file to user's directory.
+cp /etc/skel/.vimrc "${HOME}"
+
+echo '> Finished.'
+
