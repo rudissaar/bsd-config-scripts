ea1008570c1dfea6916c2e1a21b8e68b03717d40
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-12-18 03:44:03 +0000

    Improved freebsd-post-install/vim.sh script.

--- freebsd-post-install/vim.sh
+++ freebsd-post-install/vim.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env sh
+# Script that installs and configures vim editor on current system.
 
 # You need root permissions to run this script.
 if [ "$(id -u)" != '0' ]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
 # Install packages.
 pkg install -y vim-console
 
-grep -Fq 'alias vi' /etc/csh.cshrc
-
-if [ "${?}" != '0' ]; then
+if ! grep -Fq 'alias vi' /etc/csh.cshrc; then
     echo >> /etc/csh.cshrc
     echo "alias vi vim" >> /etc/csh.cshrc
 fi
 
-if [ ! -f /etc/skel ]; then
+if [ ! -d /etc/skel ]; then
     mkdir -p /etc/skel
 fi
 
@@ -25,49 +25,38 @@ if [ ! -f /etc/skel/.vimrc ]; then
 fi
 
 # Enable syntax.
-grep -Fq 'syntax on' /etc/skel/.vimrc
-
-if [ "${?}" != '0' ]; then
+if ! grep -Fq 'syntax on' /etc/skel/.vimrc; then
     echo 'syntax on' >> /etc/skel/.vimrc
 fi
 
 # Display line numbers.
-grep -Fq 'set number' /etc/skel/.vimrc
-
-if [ "${?}" != '0' ]; then
+if ! grep -Fq 'set number' /etc/skel/.vimrc; then
     echo 'set number' >> /etc/skel/.vimrc
 fi
 
 # Disable visual mode.
-grep -Fq 'set mouse-=a' /etc/skel/.vimrc
-
-if [ "${?}" != '0' ]; then
+if ! grep -Fq 'set mouse-=a' /etc/skel/.vimrc; then
     echo 'set mouse-=a' >> /etc/skel/.vimrc
 fi
 
 # Set size of tab.
-grep -Fq 'set tabstop=4' /etc/skel/.vimrc
-
-if [ "${?}" != '0' ]; then
+if ! grep -Fq 'set tabstop=4' /etc/skel/.vimrc; then
     echo 'set tabstop=4' >> /etc/skel/.vimrc
 fi
 
 # Use spaces instead of tabs.
-grep -Fq 'set expandtab' /etc/skel/.vimrc
-
-if [ "${?}" != '0' ]; then
+if ! grep -Fq 'set expandtab' /etc/skel/.vimrc; then
     echo 'set expandtab' >> /etc/skel/.vimrc
 fi
 
 # Set size of indent.
-grep -Fq 'set shiftwidth=4' /etc/skel/.vimrc
-
-if [ "${?}" != '0' ]; then
+if ! grep -Fq 'set shiftwidth=4' /etc/skel/.vimrc; then
     echo 'set shiftwidth=4' >> /etc/skel/.vimrc
 fi
 
 # Copy .vimrc file to user's directory.
 cp /etc/skel/.vimrc "${HOME}"
 
+# Let user know that script has finished its job.
 echo '> Finished.'
 
